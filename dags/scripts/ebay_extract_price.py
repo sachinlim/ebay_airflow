05f1cd5744c4cfd5f547c6f82aad166bcad8598f
Sachin Limbu <80691974+sachinlim@users.noreply.github.com>
2023-02-27 12:53:02 +0000

    adding doc strings and cleaning up code

--- dags/scripts/ebay_extract_price.py
+++ dags/scripts/ebay_extract_price.py
@@ -4,14 +4,23 @@ import statistics
 
 
 def format_search_term(link):
-    # eBay wants to use + in place of spaces in the search term
+    """
+    Formatting the search term so that it can be added onto the URL link
+    :param link: search term with spaces separating words
+    :return: search term with + instead of spaces
+    """
     formatted_search_term = link.replace(" ", "+")
 
     return formatted_search_term
 
 
 def website_data(search):
-    # URL contains search filters: "Exact words, any order", Used, Sold listings, and UK only
+    """
+    Adding the formatted search to the URL link so that BS4 knows where to look
+    URL contains search filters: "Exact words, any order", Used, Sold listings, and UK only
+    :param search: formatted search term
+    :return: data obtained from the search
+    """
     url = f'https://www.ebay.co.uk/sch/i.html?_from=R40&_nkw={search}' \
           f'&_in_kw=4&_ex_kw=&_sacat=0&LH_Sold=1&_udlo=&_udhi=&LH_ItemCondition=4&_samilow=&_samihi=' \
           f'&_stpos=M300AA&_sargn=-1%26saslc%3D1&_fsradio2=%26LH_LocatedIn%3D1&_salic=3&LH_SubLocation=1' \
@@ -23,6 +32,11 @@ def website_data(search):
 
 
 def get_data(soup):
+    """
+    Formatting the data obtained so that it contains only the data needed for mathematical analysis
+    :param soup: data obtained with website_data()
+    :return: array with data trimmed and the data associated with trimming
+    """
     products = []
     results = soup.find('div', {'class': 'srp-river-results clearfix'}).find_all('li', {'class':
                                                                                             's-item s-item__pl-on-bottom'})
@@ -46,21 +60,25 @@ def get_data(soup):
     products.sort()
     trimmed_results_list = products[trimming:-trimming]
 
-    return trimmed_results_list, trim_percentage, trimming, original_results_length
+    return trimmed_results_list
 
 
 def get_average(search_term):
+    """
+    Calculating the average if there is data for the search term
+    :param search_term: item name
+    :return: trimmed mean with the results obtained from the search
+    """
     formatted_search_term = format_search_term(search_term)
 
     soup = website_data(formatted_search_term)
-    trimmed_result_list, trim_percentage, trimming, original_results_length = get_data(soup)
+    trimmed_result_list = get_data(soup)
 
     if not trimmed_result_list:
         return 'no results'
 
     else:
         # There are sold items in the search result and the list has values
-
         trimmed_mean = statistics.mean(trimmed_result_list)
-        # minimum_value, maximum_value = self.calculate_range(trimmed_result_list)
+
         return f'{trimmed_mean:.2f}'
